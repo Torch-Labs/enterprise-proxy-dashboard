dde199294e7efc3885ea3d719de9fb2a6a832304
Sharith Godamanna <sharithg@bu.edu>
2022-07-16 00:53:21 -0400

    remove layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,17 @@
 import getConfig from "next/config";
 
-import Layout from "@/components/Layout";
-
 const { publicRuntimeConfig } = getConfig();
 const { name } = publicRuntimeConfig.site;
 
 const Home = () => {
   return (
-    <Layout>
-      <section className="py-12">
-        <div className="container mx-auto px-4">
-          <div className="prose prose-blue mx-auto">
-            <h1>{name}</h1>
-            {/* <p>Enterprise Proxy Dashboard</p> */}
-          </div>
+    <section className="py-12">
+      <div className="container mx-auto px-4">
+        <div className="prose prose-blue mx-auto">
+          <h1>{name}</h1>
         </div>
-      </section>
-    </Layout>
+      </div>
+    </section>
   );
 };
 
